18616e365fa45e0c74b64fba7d0f3d2f336d95e5
ledergec <ledergec@gmail.com>
2021-05-04 20:52:00 +0200

    Fix statuten und betriebsreglement link

--- brachland/settings.py
+++ brachland/settings.py
@@ -169,3 +169,8 @@ SERVER_URL = "www.brachland.org"
 ADMINPORTAL_NAME = "juntagrico.solawi-brachland.ch"
 ADMINPORTAL_SERVER_URL = "juntagrico.solawi-brachland.ch"
 STYLE_SHEET = "/static/brachland/css/customize.css"
+
+BYLAWS = "https://solawi-brachland.ch/wp-content/uploads/2021/03/Statuten-Solawi.pdf"
+
+BUSINESS_REGULATIONS = "https://solawi-brachland.ch/wp-content/uploads/2021/03/Betriebsreglement.pdf"
+
