e862afe42600792f5b9f0bf2f66e7ed9f32a13b9
brachland-bubikon <83664652+brachland-bubikon@users.noreply.github.com>
2021-12-08 15:41:12 +0100

    Update settings.py
    Wir ahben Server 500 Fehler und haben die Zeilen zum Debugger eingefügt.

--- brachland/settings.py
+++ brachland/settings.py
@@ -7,7 +7,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+# Temporär eingefügt zum debuggen
+ADMINS = (
+    ('Admin', 'deine_email@adresse.ch'),
+)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('JUNTAGRICO_SECRET_KEY')
