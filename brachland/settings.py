11f4ee278f9b8798680636728cbe56e0e0814c30
brachland-bubikon <83664652+brachland-bubikon@users.noreply.github.com>
2021-12-19 18:25:13 +0100

    Update settings.py
    Added django-admin-sortable2, therefore add ‘adminsortable2’ to the INSTALLED_APPS setting

--- brachland/settings.py
+++ brachland/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'juntagrico',
+    ‘adminsortable2’,
     'impersonate',
     'crispy_forms',
     'brachland',
