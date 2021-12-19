1f2e8a32a45b4dc90ab114bd8986b917b41971b9
brachland-bubikon <83664652+brachland-bubikon@users.noreply.github.com>
2021-12-19 18:27:47 +0100

    Update settings.py

--- brachland/settings.py
+++ brachland/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'juntagrico',
-    ‘adminsortable2’,
+    'adminsortable2',
     'impersonate',
     'crispy_forms',
     'brachland',
