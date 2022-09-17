cb03b7504b420068a8b68f2bc1a6fb1f70b473a8
root <root@31991.hostserv.eu>
2022-09-17 14:24:12 +0200

    install pg

--- brachland/settings.py
+++ brachland/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'juntagrico_pg',
     'juntagrico',
     'adminsortable2',
     'impersonate',
